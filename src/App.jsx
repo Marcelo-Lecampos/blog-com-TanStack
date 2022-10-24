a98f3de801832f38442018c473a1f69788b8b231
Marcelo Leão <marcelo.lecampos@gmail.com>
2022-10-24 13:15:57 -0300

    🎉 Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+import { Posts } from "./Posts";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+    <div className="App">
+      <h1>Hello world</h1>
+      <Posts />
+      <ReactQueryDevtools />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
+
+
+
+
+
