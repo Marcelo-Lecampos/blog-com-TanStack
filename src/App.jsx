31639adfa7f7e8dc27209f5ae2f2a1003bcb49d7
Marcelo Leão <marcelo.lecampos@gmail.com>
2022-10-25 17:09:35 -0300

    ♻️ refactor:  add react query no fetch de posts

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from "@tanstack/react-query";
+} from "@tanstack/react-query"; 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient();
